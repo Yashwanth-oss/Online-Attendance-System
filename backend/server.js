3fa5750724f9a850db1c3c37119405919e1765b0
Yashwanth-oss <yashwanthsd25@gmail.com>
2026-03-05 23:44:11 +0530

    successfull

--- backend/server.js
+++ backend/server.js
@@ -7,37 +7,32 @@ const authRoutes = require("./routes/authRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const attendanceRoutes = require("./routes/attendanceRoutes");
 
-
-
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected Successfully"))
-  .catch((err) => {
+.then(() => console.log("MongoDB Connected Successfully"))
+.catch((err) => {
     console.log("MongoDB Connection Error:", err);
-    process.exit(1);
-  });
-
-
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
+// Test route
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-
-// Server Start
+// IMPORTANT FOR RENDER
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
